1a9c114026b8e7703a884625a035cce112dfecdb
v-mod <80989699+vmd1@users.noreply.github.com>
2025-01-04 17:51:52 +0000

    forms

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from flask import render_template
 @app.route('/index')
 def index():
     user = {'username': 'Vivaan'}
-    return render_template('index.html', user = user, title = 'Home')
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user = user, title = 'Home', posts = posts)
